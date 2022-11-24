3f9d69921222cbe110a95af0ed179970676b9b10
Kamil Kashapov <kashapov@ispras.ru>
2022-11-24 03:32:48 +0300

    Pizza WIP

--- pizza/pizza.c
+++ pizza/pizza.c
@@ -3,26 +3,60 @@
 #include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/mman.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define SHARED_MEMORY_OBJECT_SIZE 50
+#define CHECK_RET(val)                                                         \
+    if (val < 0) {                                                             \
+        perror(#val);                                                          \
+        return val;                                                            \
+    }
+
+#define PIZZA_LEN 5 // 5 letters in "PIZZA"
+
+// Size of shared memory
+#define PIZZA_SHM_SIZE 50
+
+// Ingridient capacity
+#define INGR_CAP 4
+
+// Buffers for each ingridient
+#define P_START 0
+#define I_START (P_START + INGR_CAP)
+#define Z_START (I_START + INGR_CAP)
+#define A_START (Z_START + INGR_CAP)
+
+// Memory for conveyor
+#define CONVEYOR_START (A_START + INGR_CAP)
+#define CONVEYOR_NUM 4 // 4 Full pizzas
+#define CONVEYOR_CAP (PIZZA_LEN * CONVEYOR_NUM)
+
+#if (CONVEYOR_START + CONVEYOR_LEN) > PIZZA_SHM_SIZE
+#error Not enough space for all ingridients and conveyor.
+#endif
 
 int Recipient(void) {
     printf("Recipient reporting!\n");
     return 1;
 }
 
-int MasterChief(void) {
+int MasterChief(char *shm) {
     printf("MasterChief reporting!\n");
+
+    *(int *)shm = -1;
+
     return 0;
 }
 
-int SlaveChief(int slave_idx) {
+int SlaveChief(int slave_idx, char *shm) {
     printf("Slave #%d reporting!\n", slave_idx);
+
+    printf("SLAVE #%d: (%d)\n", slave_idx, shm[slave_idx]);
+
     return 0;
 }
 
@@ -39,34 +73,35 @@ int main(int argc, const char **argv) {
         return -1;
     }
 
-    int shm_id = shmget(IPC_PRIVATE, );
-    if (shm_fd < 0) {
-        perror("shm open");
-        return shm;
-    }
+    // Create shared memory for pizza
+    int shm_id = shmget(IPC_PRIVATE, PIZZA_SHM_SIZE, IPC_CREAT | 0600);
+    CHECK_RET(shm_id);
 
-    printf("pizza fd = (%d)\n", shm);
+    printf("pizza id = (%d)\n", shm_id);
 
-    char *pizza_addr = (char *)mmap(0, SHARED_MEMORY_OBJECT_SIZE + 1,
-                                    PROT_WRITE | PROT_READ, MAP_SHARED, shm, 0);
+    char *pizza_addr = (char *)shmat(shm_id, NULL, SHM_RND);
 
     printf("pizza addr = <%p>\n", pizza_addr);
     if (pizza_addr == (char *)(-1)) {
-        perror("mmap");
+        perror("shmat");
         return -1;
     }
+
+    // Create semaphores for each ingridient
+    int sem_id = semget(IPC_PRIVATE, 4, IPC_CREAT | 0600);
+    CHECK_RET(sem_id);
 
     // Create Master Chief
     int pid = fork();
     if (!pid) {
-        return MasterChief();
+        return MasterChief(pizza_addr);
     }
 
     // Create 4 Slaves
     for (int i = 0; i < 4; i++) {
         pid = fork();
         if (!pid) {
-            return SlaveChief(i);
+            return SlaveChief(i, pizza_addr);
         }
     }
 
@@ -74,7 +109,9 @@ int main(int argc, const char **argv) {
         pizza_num += Recipient();
     }
 
-    shm_unlink("Shared_pizza");
+    shmdt(pizza_addr);
+    shmctl(shm_id, IPC_RMID, NULL);
+    semctl(sem_id, 4, IPC_RMID, NULL);
 
     printf("All done!");
 
